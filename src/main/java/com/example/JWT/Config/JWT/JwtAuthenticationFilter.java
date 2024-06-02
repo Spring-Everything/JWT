d617bcb00e8a314af2fe5404f725cb8a042a03ad
tjdals4716 <song47166@naver.com>
2024-06-02 19:52:41 +0900

    :fire: UserDatails 구현 완료, "토큰 유효기간 갱신" 제외 JWT 전체 구현 완료

--- src/main/java/com/example/JWT/Config/JWT/JwtAuthenticationFilter.java
+++ src/main/java/com/example/JWT/Config/JWT/JwtAuthenticationFilter.java
@@ -1,9 +1,8 @@
 package com.example.JWT.Config.JWT;
 
 import com.example.JWT.Service.UserDetailsServiceImpl;
-import io.jsonwebtoken.JwtException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import io.jsonwebtoken.Claims;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -15,19 +14,17 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+
 import java.io.IOException;
 
 @Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
-    private static final Logger logger = LoggerFactory.getLogger(JwtAuthenticationFilter.class);
-    private final JwtTokenProvider jwtTokenProvider;
-    private final UserDetailsServiceImpl userDetailsService;
+    @Autowired
+    private JwtTokenProvider jwtTokenProvider;
 
-    public JwtAuthenticationFilter(JwtTokenProvider jwtTokenProvider, UserDetailsServiceImpl userDetailsService) {
-        this.jwtTokenProvider = jwtTokenProvider;
-        this.userDetailsService = userDetailsService;
-    }
+    @Autowired
+    private UserDetailsServiceImpl userDetailsService;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
@@ -43,21 +40,20 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             } else {
                 token = header;
             }
-
-            try {
-                uid = jwtTokenProvider.getUidFromToken(token);
-            } catch (JwtException e) {
-                logger.error("유효기간이 만료됐거나 인증되지 않은 토큰입니다");
-            }
+            uid = jwtTokenProvider.getUidFromToken(token);
         }
 
         if (uid != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-            UserDetails userDetails = userDetailsService.loadUserByUsername(uid);
             if (jwtTokenProvider.validateToken(token, uid)) {
-                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
-                        userDetails, null, userDetails.getAuthorities());
-                authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-                SecurityContextHolder.getContext().setAuthentication(authentication);
+                UserDetails userDetails = userDetailsService.loadUserByUsername(uid);
+                if (userDetails != null) {
+                    UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+                            userDetails, null, userDetails.getAuthorities());
+                    authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                    SecurityContextHolder.getContext().setAuthentication(authentication);
+                }
+            } else {
+                logger.error("유효기간이 만료됐거나 인증되지 않은 토큰입니다");
             }
         }
 
